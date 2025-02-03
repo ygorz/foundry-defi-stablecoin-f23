7656c7505ecae58189c8adbfc3ba8723209e22e3
George Gorzhiyev <gegorz@hotmail.com>
2025-02-02 18:55:13 -0700

    First commit, started DeFi stablecoin project

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.18;
+
+contract DecentralizedStableCoin {}
